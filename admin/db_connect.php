128fdd1f11d6dc9800cb4ba03aa0d6bbaf572928
Esteban Pulgarin Bedoya <espulgarin@hotmail.com>
2022-12-13 14:31:27 -0500

    Restablecer conexión

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,2 +1,10 @@
 <?php 
-$conn= new mysqli('localhost','root','','eras_db')or die("No se pudo hacer la conexión a la base de datos.".mysqli_error($con));
+//$conn= new mysqli('localhost','root','','eras_db')or die("No se pudo hacer la conexión a la base de datos.".mysqli_error($con));
+
+$DB_HOST = $_ENV["DB_HOST"];
+$DB_USER = $_ENV["DB_USER"];
+$DB_PASSWORD = $_ENV["DB_PASSWORD"];
+$DB_NAME = $_ENV["DB_NAME"];
+$DB_PORT = $_ENV["DB_PORT"];
+
+$conn= new mysqli($DB_HOST,$DB_USER,$DB_PASSWORD,$DB_NAME, $DB_PORT)or die("No se pudo hacer la conexión a la base de datos.".mysqli_error($con));
